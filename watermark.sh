c0ef82e23353e78bc59ad30143edb525106deb1b
SRoy <subhodeeproy89@gmail.com>
2025-09-13 23:43:39 +0000

    Update watermark.sh
    Fixed paths for Colab

--- watermark.sh
+++ watermark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BASE_DIR="/root/manga-image-translator"
+BASE_DIR="/content/manga-image-translator"
 SCRIPT="$BASE_DIR/watermark.py"
 
 if [ $# -lt 1 ]; then
@@ -8,7 +8,7 @@ if [ $# -lt 1 ]; then
 fi
 
 MODE=$1
-ROOT_DIR=${2:-"/root/Manga/Output"}
+ROOT_DIR=${2:-"/content/Manga/Output"}
 
 # Run Python script
 python3 "$SCRIPT" "$MODE" --root "$ROOT_DIR"
