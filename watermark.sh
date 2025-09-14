b50dfcea51c28d7b710d8c2afa25d90df513a010
SRoy <subhodeeproy89@gmail.com>
2025-09-14 15:10:30 +0000

    Update watermark.sh

--- watermark.sh
+++ watermark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BASE_DIR="/content/manga-image-translator"
+BASE_DIR="/kaggle/working/manga-image-translator"
 SCRIPT="$BASE_DIR/watermark.py"
 
 if [ $# -lt 1 ]; then
@@ -8,7 +8,7 @@ if [ $# -lt 1 ]; then
 fi
 
 MODE=$1
-ROOT_DIR=${2:-"/content/Manga/Output"}
+ROOT_DIR=${2:-"/kaggle/working/Manga/Output"}
 
 # Run Python script
 python3 "$SCRIPT" "$MODE" --root "$ROOT_DIR"
